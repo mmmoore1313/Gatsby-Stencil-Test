935ba15f0f5b121d1b6a4ee33d18b75e087f4ffa
Joe Torreggiani <joetorreggiani@gmail.com>
2021-10-30 17:41:08 -0400

    Get stencil polyfills working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,13 @@
-// Step 1: Import React
-import * as React from 'react'
-import 'spec-ods'
-
-// Step 2: Define your component
-
-const array = [
-  {
-    label: 'Accordion 1',
-    description: 'Lorem ipsum',
-    color: '#439ECA',
-    width: '300px'
-  },
-  {
-    label: 'Accordion 2',
-    description: 'Lorem ipsum',
-    color: '#7EC74A',
-    width: '300px'
-  },
-  {
-    label: 'Accordion 3',
-    description: 'Lorem ipsum',
-    color: '#F8CD41',
-    width: '300px'
-  }
-]
+import React from 'react'
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <plain-button>Hello</plain-button>
-      <div className="App">
-        {
-          array.map((array) => {
-            return (
-              <my-accordion 
-              label={array.label}
-              description={array.description}
-              color={array.color}
-              width={array.width}>
-              </my-accordion>
-            )})
-        }
-      </div>
+      <plain-button>Click Me!</plain-button>
     </main>
   )
 }
-// Step 3: Export your component
+
 export default IndexPage
