af9e2351c590349d42210d48be292498ea7c1523
Matt Moore <Matt.M.Moore13@outlook.com>
2021-10-30 16:53:35 -0400

    Work in progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "GatsbyStencilTest",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-stencil`,
+      options: {
+        // The module of your components (required), eg "@ionic/core".
+        // Add multiple modules by using an array eg ["@ionic/core", "my-stencil-components"]
+        module: "spec-ods",
+
+        // Stencil renderToString options (optional): https://stenciljs.com/docs/hydrate-app#configuration-options
+        renderToStringOptions: {
+          prettyHtml: true,
+        },
+      },
+    },
+  ],
 };
